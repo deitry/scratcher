a04e5e32fba4d886cc622dff08b32df33a40c942
vornichev <vornichev@elins.ru>
2019-02-09 00:45:20 +0300

    Portaudio: initializing portaudio http://www.portaudio.com/docs/v19-doxydocs/initializing_portaudio.html

--- main.cpp
+++ main.cpp
@@ -5,5 +5,21 @@
 int main(int argc, char** argv)
 {
     std::cout << "Hello, world!" << std::endl;
+
+    // инициализируем PortAudio
+    auto err = Pa_Initialize();
+    if (err != paNoError) {
+        printf("PortAudio error: %s\n", Pa_GetErrorText(err));
+        return 0; // больше делать нечего
+    }
+
+    // ... work ...
+
+    // завершаем работу PortAudio
+    err = Pa_Terminate();
+    if (err != paNoError) {
+        printf("PortAudio error: %s\n", Pa_GetErrorText(err));
+    }
+
     return 0;
 }
