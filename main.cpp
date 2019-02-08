7ee4ec65625829d6edfa75288d5fe840fd469785
vornichev <vornichev@elins.ru>
2019-02-09 00:57:22 +0300

    Базовая работа с аудиопотоком http://www.portaudio.com/docs/v19-doxydocs/open_default_stream.html http://www.portaudio.com/docs/v19-doxydocs/start_stop_abort.html

--- main.cpp
+++ main.cpp
@@ -2,6 +2,10 @@
 
 #include "src/simple_audio_source.hpp"
 
+
+#define SAMPLE_RATE (44100)
+static paTestData data;
+
 int main(int argc, char** argv)
 {
     std::cout << "Hello, world!" << std::endl;
@@ -13,7 +17,41 @@ int main(int argc, char** argv)
         return 0; // больше делать нечего
     }
 
-    // ... work ...
+    // открываем аудиопоток
+    PaStream *stream;
+    err = Pa_OpenDefaultStream( &stream,
+                                0,          /* no input channels */
+                                2,          /* stereo output */
+                                paFloat32,  /* 32 bit floating point output */
+                                SAMPLE_RATE,
+                                256,        /* frames per buffer, i.e. the number
+                                                of sample frames that PortAudio will
+                                                request from the callback. Many apps
+                                                may want to use
+                                                paFramesPerBufferUnspecified, which
+                                                tells PortAudio to pick the best,
+                                                possibly changing, buffer size.*/
+                                patestCallback, /* this is your callback function */
+                                &data ); /*This is a pointer that will be passed to
+                                                your callback*/
+    if (err != paNoError) {
+        printf("PortAudio error: %s\n", Pa_GetErrorText(err));
+    }
+
+    // запускаем поток
+    err = Pa_StartStream(stream);
+    if (err != paNoError) {
+        printf("PortAudio error: %s\n", Pa_GetErrorText(err));
+    }
+
+    // немного ждём
+    Pa_Sleep(1*1000); // в мс
+
+    // останавливаем поток
+    err = Pa_StopStream(stream);
+    if (err != paNoError) {
+        printf("PortAudio error: %s\n", Pa_GetErrorText(err));
+    }
 
     // завершаем работу PortAudio
     err = Pa_Terminate();
